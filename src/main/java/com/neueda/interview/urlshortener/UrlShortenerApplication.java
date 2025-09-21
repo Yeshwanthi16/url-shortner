6953f1a4a0fcd60cdedf3862a15f158e884e23e8
Yeshwanthi16 <yashuin@gmail.com>
2025-09-21 11:29:10 +0530

    first commit

---
+++ src/main/java/com/neueda/interview/urlshortener/UrlShortenerApplication.java
@@ -0,0 +1,15 @@
+package com.neueda.interview.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = "com.neueda.interview.urlshortener.repository")
+@SpringBootApplication
+public class UrlShortenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortenerApplication.class, args);
+	}
+
+}
